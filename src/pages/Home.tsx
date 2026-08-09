730a351074ecc05fb05cec5cd3192f1380cb9db3
Yonas19 <yonjr0936@gmail.com>
2026-08-09 22:19:32 +0400

    fixed the import errors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router";
 import logoImg from "@/imports/1-removebg-preview.png";
-import heroBg from "@/imports/hero2__1___1_.mp4";
-import aboutVid from "@/imports/1003__3_-copy-copy-1.mp4";
-import bts1 from "../imports/IMG_0285.png";
-import bts2 from "../imports/IMG_0286.png";
-import bts3 from "../imports/IMG_0287.png";
-import bts4 from "../imports/IMG_0288.png";
-import spotlightImg from "../imports/IMG_0292.png";
+import heroBg from "src/imports/hero2__1___1_.mp4";
+import aboutVid from "src/imports/IMG_0285.png";
+import bts1 from "src/imports/IMG_0285.png";
+import bts2 from "src/imports/IMG_0286.png";
+import bts3 from "src/imports/IMG_0287.png";
+import bts4 from "src/imports/IMG_0288.png";
+import spotlightImg from "src/imports/IMG_0292.png";
 import { SERVICES, ServiceCard } from "@/shared";
 
 const PREVIEW_VIDEOS = [
